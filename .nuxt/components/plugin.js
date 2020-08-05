654330f66af72764e6a1be1829200cab15071fbe
Gustavo Grauzely da Silva Alves <ggrauzely@gmail.com>
2020-08-05 11:25:31 -0400

    Ajustes scrollBehavior e componentes enviados para a pasta global

--- .nuxt/components/plugin.js
+++ .nuxt/components/plugin.js
@@ -1,6 +1,14 @@
 import Vue from 'vue'
 
 const globalComponents = {
+  AppSearchInput: () => import('../..\\components\\global\\AppSearchInput.vue' /* webpackChunkName: "components_global/AppSearchInput" */).then(c => c.default || c),
+  Card: () => import('../..\\components\\global\\Card.vue' /* webpackChunkName: "components_global/Card" */).then(c => c.default || c),
+  Dropdown: () => import('../..\\components\\global\\Dropdown.vue' /* webpackChunkName: "components_global/Dropdown" */).then(c => c.default || c),
+  Footer: () => import('../..\\components\\global\\Footer.vue' /* webpackChunkName: "components_global/Footer" */).then(c => c.default || c),
+  NavBar: () => import('../..\\components\\global\\NavBar.vue' /* webpackChunkName: "components_global/NavBar" */).then(c => c.default || c),
+  PageCategory: () => import('../..\\components\\global\\PageCategory.vue' /* webpackChunkName: "components_global/PageCategory" */).then(c => c.default || c),
+  Paginator: () => import('../..\\components\\global\\Paginator.vue' /* webpackChunkName: "components_global/Paginator" */).then(c => c.default || c),
+  PrevNext: () => import('../..\\components\\global\\PrevNext.vue' /* webpackChunkName: "components_global/PrevNext" */).then(c => c.default || c)
 }
 
 for (const name in globalComponents) {
