8b29fab877ab1eb452259e146451751a16aa1de7
Gustavo Grauzely da Silva Alves <ggrauzely@gmail.com>
2020-08-11 16:49:08 -0400

    Configurações PWA

--- .nuxt/nuxt-icons.js
+++ .nuxt/nuxt-icons.js
@@ -1,5 +1,5 @@
 export default async function (ctx, inject) {
-  const icons = {"64x64":"/_nuxt/icons/icon_64.ffd94b.png","120x120":"/_nuxt/icons/icon_120.ffd94b.png","144x144":"/_nuxt/icons/icon_144.ffd94b.png","152x152":"/_nuxt/icons/icon_152.ffd94b.png","192x192":"/_nuxt/icons/icon_192.ffd94b.png","384x384":"/_nuxt/icons/icon_384.ffd94b.png","512x512":"/_nuxt/icons/icon_512.ffd94b.png"}
+  const icons = {"64x64":"/_nuxt/icons/icon_64.8ce846.png","120x120":"/_nuxt/icons/icon_120.8ce846.png","144x144":"/_nuxt/icons/icon_144.8ce846.png","152x152":"/_nuxt/icons/icon_152.8ce846.png","192x192":"/_nuxt/icons/icon_192.8ce846.png","384x384":"/_nuxt/icons/icon_384.8ce846.png","512x512":"/_nuxt/icons/icon_512.8ce846.png"}
   const getIcon = size => icons[size + 'x' + size] || ''
   inject('icon', getIcon)
 }
--- nuxt.config.js
+++ nuxt.config.js
@@ -14,7 +14,7 @@ export default {
    ** See https://nuxtjs.org/api/configuration-head
    */
   head: {
-    title: 'Blog - GrauZ Tecnologia',
+    title: 'GrauZ Knowlegde',
     meta: [
       { charset: 'utf-8' },
       { name: 'viewport', content: 'width=device-width, initial-scale=1' },
@@ -23,8 +23,71 @@ export default {
         name: 'description',
         content: process.env.npm_package_description || '',
       },
+      { mobileAppIOS: true },
     ],
-    link: [{ rel: 'icon', type: 'image/x-icon', href: '/favicon.ico' }],
+    link: [
+      { rel: 'icon', type: 'image/x-icon', href: 'favicon.ico' },
+      {
+        rel: 'apple-touch-startup-image',
+        href: 'iphone5_splash.png',
+        media:
+          '(device-width: 320px) and (device-height: 568px) and (-webkit-device-pixel-ratio: 2)',
+      },
+      {
+        rel: 'apple-touch-startup-image',
+        href: 'iphone6_splash.png',
+        media:
+          '(device-width: 375px) and (device-height: 667px) and (-webkit-device-pixel-ratio: 2)',
+      },
+      {
+        rel: 'apple-touch-startup-image',
+        href: 'iphoneplus_splash.png',
+        media:
+          '(device-width: 621px) and (device-height: 1104px) and (-webkit-device-pixel-ratio: 3)',
+      },
+      {
+        rel: 'apple-touch-startup-image',
+        href: 'iphonex_splash.png',
+        media:
+          '(device-width: 375px) and (device-height: 812px) and (-webkit-device-pixel-ratio: 3)',
+      },
+      {
+        rel: 'apple-touch-startup-image',
+        href: 'iphonexr_splash.png',
+        media:
+          '(device-width: 414px) and (device-height: 896px) and (-webkit-device-pixel-ratio: 2)',
+      },
+      {
+        rel: 'apple-touch-startup-image',
+        href: 'iphonexsmax_splash.png',
+        media:
+          '(device-width: 414px) and (device-height: 896px) and (-webkit-device-pixel-ratio: 3)',
+      },
+      {
+        rel: 'apple-touch-startup-image',
+        href: 'ipad_splash.png',
+        media:
+          '(device-width: 768px) and (device-height: 1024px) and (-webkit-device-pixel-ratio: 2)',
+      },
+      {
+        rel: 'apple-touch-startup-image',
+        href: 'ipadpro1_splash.png',
+        media:
+          '(device-width: 834px) and (device-height: 1112px) and (-webkit-device-pixel-ratio: 2)',
+      },
+      {
+        rel: 'apple-touch-startup-image',
+        href: 'ipadpro3_splash.png',
+        media:
+          '(device-width: 834px) and (device-height: 1194px) and (-webkit-device-pixel-ratio: 2)',
+      },
+      {
+        rel: 'apple-touch-startup-image',
+        href: 'ipadpro2_splash.png',
+        media:
+          '(device-width: 1024px) and (device-height: 1366px) and (-webkit-device-pixel-ratio: 2)',
+      },
+    ],
   },
   /*
    ** Global CSS
@@ -76,9 +139,11 @@ export default {
   loading: { color: '#b22727' },
   pwa: {
     manifest: {
-      name: 'Grauz Blog',
-      short_name: 'Grauz Blog',
+      name: 'Grauz Knowlegde',
+      short_name: 'Grauz Knowlegde',
       lang: 'pt-br',
+      theme_color: '#272729',
+      background_color: '#272729',
       useWebmanifestExtension: false,
     },
   },
